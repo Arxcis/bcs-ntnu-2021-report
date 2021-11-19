d78d820a9dbc47a88b66e8bfb46b19c88a51b5d1
Jonas Johan Solsvik <jonas@symfoni.solutions>
2021-11-19 16:53:01 +0100

    remove \passthrough

--- markdown-to-latex.py
+++ markdown-to-latex.py
@@ -1,4 +1,5 @@
 import sys
+import re
 
 CHAPTERS = ["1-introduction",
             "2-development-process",
@@ -20,11 +21,30 @@ def main():
 
     # 2. Rewrite "".svg" -> ""
     content = content.replace(".svg", "")
+    content = replace_passthrough(content)
 
     file.seek(0)
     file.write(content)
     file.truncate()
     file.close()
+
+
+def replace_passthrough(string):
+    string = re.sub(r'\\passthrough\{', "", string)
+    string = re.sub(r'!\}', "!", string)
+    return string
+
+
+def test_replace_passthrough():
+    TESTS = [
+        "  \passthrough{\lstinline!<didname>!} to \passthrough{\lstinline!<did>!}",
+        "  Show the did of a single \passthrough{\lstinline!<didname>!}.",
+        "\passthrough{\lstinline!did!} in the command-line. The user has to make",
+        "looks after running \passthrough{\lstinline!did init!} :"
+    ]
+
+    for test in TESTS:
+        print(replace_passthrough(test))
 
 
 if __name__ == "__main__":
